380f9c711de317c32a736eab06350a1ab4ab447c
ThoPham02 <2021050819@student.humg.edu.vn>
2024-06-20 19:35:41 +0700

    tmp

--- common/response.go
+++ common/response.go
@@ -2,11 +2,18 @@ package common
 
 // Common response code
 const (
-	SUCCESS_CODE = 0
+	SUCCESS_CODE    = 0
 	SUCCESS_MESSAGE = "Success"
 
-	INVALID_REQUEST_CODE = 1
+	INVALID_REQUEST_CODE    = 1
 	INVALID_REQUEST_MESSAGE = "Invalid request"
+
+	DB_ERROR_CODE    = 2
+	DB_ERROR_MESSAGE = "Database error"
 )
 
 // Account service response code from 10000-19999
+const (
+	USER_ALREADY_EXISTS_CODE    = 10000
+	USER_ALREADY_EXISTS_MESSAGE = "User already exists"
+)
--- service/account/api/internal/svc/servicecontext.go
+++ service/account/api/internal/svc/servicecontext.go
@@ -2,14 +2,19 @@ package svc
 
 import (
 	"roomrover/service/account/api/internal/config"
+	"roomrover/service/account/model"
+
+	"github.com/zeromicro/go-zero/core/stores/sqlx"
 )
 
 type ServiceContext struct {
-	Config config.Config
+	Config    config.Config
+	UserModel model.UsersModel
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
-		Config: c,
+		Config:    c,
+		UserModel: model.NewUsersModel(sqlx.NewMysql(c.DataSource)),
 	}
 }
